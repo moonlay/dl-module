a32fd621022321f88d94734c45af86d4b9eb2ce7
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-08-09 17:01:39 +0700

    add Master Textile

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ module.exports = {
             ProductManager: require("./src/managers/core/product-manager"),
             BuyerManager: require("./src/managers/core/buyer-manager"),
             SupplierManager: require("./src/managers/core/supplier-manager"),
+        },
+        master: {
+            product: {
+                TextileManager: require('./src/managers/core/textile-manager')
+            }
         }
     }
 }
