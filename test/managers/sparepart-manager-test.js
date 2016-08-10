11cfdb0cb5c94232d5466daab1899e8879ef122e
SunlikeSiagian <sunlike.siagian@moonlay.com>
2016-08-10 16:41:31 +0700

    Update sparepart module

--- test/managers/sparepart-manager-test.js
+++ test/managers/sparepart-manager-test.js
@@ -1,11 +1,28 @@
 var helper = require("../helper");
 var SparepartManager = require("../../src/managers/core/sparepart-manager");
 var instanceManager = null;
-require("should");
+var should = require("should");
 
 function getData() {
     var Sparepart = require('dl-models').core.Sparepart;
+    var UoM = require('dl-models').core.UoM;
+    var UoM_Template = require('dl-models').core.UoM_Template;
+
     var sparepart = new Sparepart();
+    var uom_template = new UoM_Template({
+        mainValue: 1,
+        mainUnit: 'M',
+        convertedValue: 1,
+        convertedUnit: 'M'
+    });
+     var _uom_units = [];
+    _uom_units.push(uom_template);
+
+    var uom = new UoM({
+        category: 'UoM_Unit_Test',
+        default: uom_template,
+        units: _uom_units
+    });
 
     var now = new Date();
     var stamp = now / 1000 | 0;
@@ -14,6 +31,7 @@ function getData() {
     sparepart.code = code;
     sparepart.name = `name[${code}]`;
     sparepart.description = `description for ${code}`;
+    sparepart.UoM = uom;
 
     return sparepart;
 
