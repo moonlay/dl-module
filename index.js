ac887a4b76f25e60f01a5ead9dd2b39f36379c86
SunlikeSiagian <sunlike.siagian@moonlay.com>
2016-08-10 15:37:17 +0700

    resolve conflict

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
     managers: {
         core: {
-            ProductManager: require("./src/managers/core/product-manager"),
             BuyerManager: require("./src/managers/core/buyer-manager"),
             SupplierManager: require("./src/managers/core/supplier-manager"),
-            SparepartManager: require("./src/manager/core/sparepart-manager")
+            SparepartManager: require("./src/manager/core/sparepart-manager"),
+            AccessoriesManager: require("./src/managers/core/accessories-manager"),
+            TextileManager: require('./src/managers/core/textile-manager'),
+            FabricManager: require('./src/managers/core/fabric-manager')
         }
     }
 }
