e0a33085c01df0eaf360a58105da5055f6f0d823
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-03 16:58:08 +0700

    update index

--- test/index.js
+++ test/index.js
@@ -14,13 +14,6 @@ describe('#dl-module', function (done) {
     // test('@AUTH/ROLE', './auth/role');
     // test('@AUTH/API-ENDPOINT', './auth/api-endpoint');
 
-    test('@ETL/DIM-CATEGORY', './etl/dim-category');
-    // test('@ETL/DIM-DIVISION', './etl/dim-division');
-    test('@ETL/DIM-SUPPLIER', './etl/dim-supplier');
-    // test('@ETL/DIM-UNIT', './etl/dim-unit');
-    // test('@ETL/FACT-TOTAL-HUTANG', './etl/fact-total-hutang');
-    test('@ETL/FACT-PURCHASING', './etl/fact-purchasing');    
-
     // Master
     test('@MASTER/ACCOUNT-BANK', './master/account-bank');
     test('@MASTER/BUDGET', './master/budget');
@@ -49,14 +42,14 @@ describe('#dl-module', function (done) {
     //Purchasing 
     test('@PURCHASING/PURCHASE REQUEST', './purchasing/purchase-request');
     test('@PURCHASING/PURCHASE ORDER', './purchasing/purchase-order');
-    test('@PURCHASING/PURCHASE ORDER EXTERNAL', './purchasing/purchase-order-external'); 
-    test('@PURCHASING/DELIVERY ORDER', './purchasing/delivery-order'); 
-    test('@PURCHASING/UNIT RECEIPT NOTE', './purchasing/unit-receipt-note'); 
-    test('@PURCHASING/UNIT PAYMENT ORDER', './purchasing/unit-payment-order'); 
+    test('@PURCHASING/PURCHASE ORDER EXTERNAL', './purchasing/purchase-order-external');
+    test('@PURCHASING/DELIVERY ORDER', './purchasing/delivery-order');
+    test('@PURCHASING/UNIT RECEIPT NOTE', './purchasing/unit-receipt-note');
+    test('@PURCHASING/UNIT PAYMENT ORDER', './purchasing/unit-payment-order');
     test('@PURCHASING/UNIT PAYMENT PRICE CORRECTION', './purchasing/unit-payment-price-correction-note');
     test('@PURCHASING/UNIT PAYMENT QUANTITY CORRECTION', './purchasing/unit-payment-quantity-correction-note');
-    
-     test('@purchasing/purchase-order/report', './purchasing/purchase-order/report/report');		
+
+    test('@purchasing/purchase-order/report', './purchasing/purchase-order/report/report');
 
     // ok
     // test('@purchasing/delivery-order-manager', './purchasing/delivery-order-manager-test');
@@ -86,7 +79,15 @@ describe('#dl-module', function (done) {
     // test('@production/winding-quality-sampling-manager', './production/spinning/winding/winding-quality-sampling-manager-test');
     // test('@production/winding-production-output-manager', './production/spinning/winding/winding-production-output-manager-test');
     test('@production/daily-operation', './production/finishing-printing/daily-operation');
-    
+
     //Sales
     test('@production/production-order', './sales/production-order/create');
+
+    // etl
+    test('@ETL/DIM-CATEGORY', './etl/dim-category');
+    // test('@ETL/DIM-DIVISION', './etl/dim-division');
+    test('@ETL/DIM-SUPPLIER', './etl/dim-supplier');
+    // test('@ETL/DIM-UNIT', './etl/dim-unit');
+    // test('@ETL/FACT-TOTAL-HUTANG', './etl/fact-total-hutang');
+    test('@ETL/FACT-PURCHASING', './etl/fact-purchasing');
 });
