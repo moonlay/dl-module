049da3e58097d858ceae59a73ba75df5545b5665
Leslie Aula <cristiano.ulo@gmail.com>
2017-03-07 17:41:00 +0700

    update

--- src/etl/fact-production-order-etl-manager.js
+++ src/etl/fact-production-order-etl-manager.js
@@ -62,7 +62,7 @@ module.exports = class FactProductionOrderEtlManager extends BaseManager {
     }
 
     extract(time) {
-        var timestamp = new Date(time[0].start);
+        var timestamp = new Date(1970, 1, 1);
         return this.productionOrderManager.collection.find({
             _deleted: false,
             _updatedDate: {
