61c1bc426f4a6753af2b50bfd31b30aa542f2b42
achensetyawati <achenzetya@gmail.com>
2017-04-29 11:43:27 +0700

    fix unit test

--- test/sales/production-order/validator.js
+++ test/sales/production-order/validator.js
@@ -21,7 +21,7 @@ before('#00. connect db', function(done) {
 });
 
 
-it('#02. should error when create new data with shippingQuantityTolerance more than 100', function (done) {
+it('#01. should error when create new data with shippingQuantityTolerance more than 100', function (done) {
     dataUtil.getNewData()
         .then(sc => {
 
@@ -46,54 +46,16 @@ it('#02. should error when create new data with shippingQuantityTolerance more t
         });
 });
 
-it('#03. should error when create new data with RUN but without RUNWidth', function (done) {
-    dataUtil.getNewData()
-        .then(sc => {
 
-            sc.RUN = "1 RUN";
-            sc.RUNWidth[0]=0;
 
-            manager.create(sc)
-                .then(id => {
-                    done("should error when create new data with RUN but without RUNWidth");
-                })
-                .catch(e => {
-                    try {
-                        e.errors.should.have.property('RUNWidth');
-                        done();
-                    }
-                    catch (ex) {
-                        done(ex);
-                    }
-                });
-        })
-        .catch(e => {
-            done(e);
-        });
-});
-
-it('#04. it should error when create new data with different total quantity', function (done) {
+it('#02. it should error when create new data with different total quantity', function (done) {
     dataUtil.getNewData()
         .then(sc => {
             sc.orderQuantity=100;
             sc.detail = [{
-                        code:`code1/${codeGenerator()}`,
-                        colorTypeId:color1._id,
-                        colorType:color1,
-                        colorRequest:`reddish`,
-                        colorTemplate:`template1`,
                         quantity:10,
-                        uomId: _uom._id,
-                        uom:_uom,
                     }, {
-                        code:`code2/${codeGenerator()}`,
-                        colorTypeId:color2._id,
-                        colorType:color2,
-                        colorRequest:`gray`,
-                        colorTemplate:`template2`,
                         quantity:5,
-                        uomId: _uom._id,
-                        uom:_uom,
                     }];
 
             manager.create(sc)
