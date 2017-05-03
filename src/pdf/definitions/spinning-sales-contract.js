6e9a5438a01ad6505ca86de53a9d1e67e09f230b
achensetyawati <achenzetya@gmail.com>
2017-05-03 08:39:06 +0700

    fix print pdf SC

--- src/pdf/definitions/spinning-sales-contract.js
+++ src/pdf/definitions/spinning-sales-contract.js
@@ -56,14 +56,15 @@ module.exports = function (salesContract) {
 
     var amountbyCurrency=`${parseFloat(amount).toLocaleString(locale, locale.currency)}`;
 
-    if(amount % 1 !=0){
-        amount=parseFloat(amount.toFixed(2));
-    }
+    
 
     if(salesContract.accountBank.currency.code.toLowerCase()=="usd"){
         price=`${parseFloat(salesContract.price).toLocaleString(locale, locale.currencySalesContract)}`;
         amount=parseFloat(amount.toFixed(3));
         amountbyCurrency=`${parseFloat(amount).toLocaleString(locale, locale.currencySalesContract)}`;
+    }
+    else if(amount % 1 !=0){
+        amount=parseFloat(amount.toFixed(2));
     }
 
     var detail = salesContract.accountBank.currency.symbol + " " + price + ' / KG' + "\n";
