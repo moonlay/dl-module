cc49f4c41d891220a96250775f35c8ac44e01bad
jackyrusly <jacky.rusly@CARTIER.moonlay.jakarta>
2017-05-26 09:20:19 +0700

    ETL Inventory Summary & Inventory Movement & Dim Product & Dim Storage

---
+++ src/etl/dim-storage-etl-manager.js
@@ -0,0 +1,187 @@
+'use strict'
+
+// external deps 
+var ObjectId = require("mongodb").ObjectId;
+var BaseManager = require('module-toolkit').BaseManager;
+var moment = require("moment");
+
+// internal deps 
+require('mongodb-toolkit');
+
+var StorageManager = require('../managers/master/storage-manager');
+
+module.exports = class DimStorageEtlManager extends BaseManager {
+    constructor(db, user, sql) {
+        super(db, user);
+        this.sql = sql;
+        this.storageManager = new StorageManager(db, user);
+        this.migrationLog = this.db.collection("migration-log");
+    }
+
+    run() {
+        var startedDate = new Date();
+        this.migrationLog.insert({
+            description: "Dim Storage from MongoDB to Azure DWH",
+            start: startedDate
+        })
+        return this.getTimeStamp()
+            .then((time) => this.extract(time))
+            .then((data) => this.transform(data))
+            .then((data) => this.load(data))
+            .then((result) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: "Dim Storage from MongoDB to Azure DWH",
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: "Successful"
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            })
+            .catch((err) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: "Dim Storage from MongoDB to Azure DWH",
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: err
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            });
+    }
+
+    getTimeStamp() {
+        return this.migrationLog.find({
+            description: "Dim Storage from MongoDB to Azure DWH",
+            status: "Successful"
+        }).sort({
+            finishedDate: -1
+        }).limit(1).toArray()
+    }
+
+    extract(time) {
+        var timestamp = new Date(time[0].start);
+        // var timestamp = new Date(1970, 1, 1);
+        return this.storageManager.collection.find({
+            _updatedDate: {
+                "$gt": timestamp
+            },
+            _deleted: false
+        }).toArray();
+    }
+
+    transform(data) {
+        var result = data.map((item) => {
+            return {
+                storageCode: item.code ? `'${item.code.replace(/'/g, '"')}'` : null,
+                storageName: item.name ? `'${item.name.replace(/'/g, '"')}'` : null,
+                storageDescription: item.description ? `'${item.description.replace(/'/g, '"')}'` : null,
+                deleted: `'${item._deleted}'`, 
+            };
+        });
+        return Promise.resolve([].concat.apply([], result));
+    }
+
+    insertQuery(sql, query) {
+        return new Promise((resolve, reject) => {
+            sql.query(query, function(err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            })
+        })
+    }
+
+    load(data) {
+        return new Promise((resolve, reject) => {
+            this.sql.startConnection()
+                .then(() => {
+
+                    var transaction = this.sql.transaction();
+
+                    transaction.begin((err) => {
+
+                        var request = this.sql.transactionRequest(transaction);
+
+                        var command = [];
+
+                        var sqlQuery = '';
+
+                        var count = 1;
+
+                        for (var item of data) {
+                            if (item) {
+                                var queryString = `INSERT INTO [DL_Dim_Storage_Temp]([code], [name], [description], [Deleted]) 
+                                                    VALUES(${item.storageCode}, ${item.storageName}, ${item.storageDescription}, ${item.deleted}) ;\n`;
+                                sqlQuery = sqlQuery.concat(queryString);
+                                if (count % 1000 === 0) {
+                                    command.push(this.insertQuery(request, sqlQuery));
+                                    sqlQuery = "";
+                                }
+                                console.log(`add data to query  : ${count}`);
+                                count++;
+                            }
+                        }
+
+
+                        if (sqlQuery !== "")
+
+                            command.push(this.insertQuery(request, `${sqlQuery}`));
+
+                        this.sql.multiple = true;
+
+                        return Promise.all(command)
+                            .then((results) => {
+                                request.execute("DL_UPSERT_DIM_STORAGE").then((execResult) => {
+                                    request.execute("DL_INSERT_DIMTIME").then((execResult) => {
+                                        transaction.commit((err) => {
+                                            if (err)
+                                                reject(err);
+                                            else
+                                                resolve(results);
+                                        });
+                                    }).catch((error) => {
+                                        transaction.rollback((err) => {
+                                            console.log("rollback")
+                                            if (err)
+                                                reject(err)
+                                            else
+                                                reject(error);
+                                        });
+                                    })
+                                }).catch((error) => {
+                                    transaction.rollback((err) => {
+                                        console.log("rollback")
+                                        if (err)
+                                            reject(err)
+                                        else
+                                            reject(error);
+                                    });
+                                })
+                            })
+                            .catch((error) => {
+                                transaction.rollback((err) => {
+                                    console.log("rollback");
+                                    if (err)
+                                        reject(err)
+                                    else
+                                        reject(error);
+                                });
+                            });
+                    })
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
+            .catch((err) => {
+                reject(err);
+            })
+    }
+}
---
+++ src/etl/fact-inventory-summary-etl-manager.js
@@ -0,0 +1,200 @@
+'use strict'
+
+// external deps 
+var ObjectId = require("mongodb").ObjectId;
+var BaseManager = require("module-toolkit").BaseManager;
+var moment = require("moment");
+
+// internal deps 
+require("mongodb-toolkit");
+
+var InventorySummaryManager = require("../managers/inventory/inventory-summary-manager");
+const MIGRATION_LOG_DESCRIPTION = "Fact Inventory Summary from MongoDB to Azure DWH"
+
+module.exports = class FactInventorySummaryManager extends BaseManager {
+    constructor(db, user, sql) {
+        super(db, user);
+        this.sql = sql;
+        this.inventorySummaryManager = new InventorySummaryManager(db, user);
+        this.migrationLog = this.db.collection("migration-log");
+    }
+
+    run() {
+        var startedDate = new Date()
+        this.migrationLog.insert({
+            description: MIGRATION_LOG_DESCRIPTION,
+            start: startedDate,
+        })
+        return this.timestamp()
+            .then((time) => this.extract(time))
+            .then((data) => this.transform(data))
+            .then((data) => this.load(data))
+            .then((results) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: MIGRATION_LOG_DESCRIPTION,
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: "Successful"
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            })
+            .catch((err) => {
+                var finishedDate = new Date();
+                var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
+                var updateLog = {
+                    description: MIGRATION_LOG_DESCRIPTION,
+                    start: startedDate,
+                    finish: finishedDate,
+                    executionTime: spentTime + " minutes",
+                    status: err
+                };
+                this.migrationLog.updateOne({ start: startedDate }, updateLog);
+            });
+    };
+
+    timestamp() {
+        return this.migrationLog.find({
+            description: MIGRATION_LOG_DESCRIPTION,
+            status: "Successful"
+        }).sort({ finish: -1 }).limit(1).toArray()
+    }
+
+    extract(time) {
+        var timestamp = new Date(time[0].start);
+        // var timestamp = new Date(1970, 1, 1);
+        return this.inventorySummaryManager.collection.find({
+            _updatedDate: {
+                $gt: timestamp
+            }
+        }).toArray();
+    }
+
+    transform(data) {
+        var result = data.map((item) => {
+
+            return {
+                storageCode: item.storageCode ? `'${item.storageCode.replace(/'/g, '"')}'` : null,
+                storageName: item.storageName ? `'${item.storageName.replace(/'/g, '"')}'` : null,
+                qty: item.quantity,
+                productCode: item.productCode ? `'${item.productCode.replace(/'/g, '"')}'` : null,
+                productName: item.productName ? `'${item.productName.replace(/'/g, '"')}'` : null,
+                uom: item.uom ? `'${item.uom.replace(/'/g, '"')}'` : null,
+                deleted: `'${item._deleted}'`,
+                code: item.code ? `'${item.code.replace(/'/g, '"')}'` : null
+            }
+        });
+        return Promise.resolve([].concat.apply([], result));
+    };
+
+    insertQuery(sql, query) {
+        return new Promise((resolve, reject) => {
+            sql.query(query, function (err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                };
+            });
+        });
+    };
+
+    load(data) {
+        return new Promise((resolve, reject) => {
+            this.sql.startConnection()
+                .then(() => {
+
+                    var transaction = this.sql.transaction();
+
+                    transaction.begin((err) => {
+
+                        var request = this.sql.transactionRequest(transaction);
+
+                        var command = [];
+
+                        var sqlQuery = '';
+
+                        var count = 1;
+
+                        for (var item of data) {
+                            if (item) {
+                                var values = `${item.storageCode}, ${item.uom}, ${item.productCode}, ${item.qty}, ${item.deleted}, ${item.code}, ${item.storageName}, ${item.productName}`;
+                                var queryString = sqlQuery === "" ? `INSERT INTO [DL_Fact_Inventory_Summary_Temp]([Storage Code], [UOM], [Product Code], [Quantity], [Deleted], [Code], [Storage Name], [Product Name]) VALUES(${values})` : `,(${values})`;
+                                
+                                sqlQuery = sqlQuery.concat(queryString);
+                                if (count % 1000 === 0) {
+                                    command.push(this.insertQuery(request, sqlQuery));
+                                    sqlQuery = "";
+                                }
+                                console.log(`add data to query  : ${count}`);
+                                count++;
+                            }
+                        }
+
+                        if (sqlQuery != "")
+                            command.push(this.insertQuery(request, `${sqlQuery}`));
+
+                        this.sql.multiple = true;
+
+                        // var fs = require("fs");
+                        // var path = "C:\\Users\\jacky.rusly\\Desktop\\Log.txt";
+
+                        // fs.writeFile(path, sqlQuery, function (error) {
+                        //     if (error) {
+                        //         console.log("write error:  " + error.message);
+                        //     } else {
+                        //         console.log("Successful Write to " + path);
+                        //     }
+                        // });
+
+                        return Promise.all(command)
+                            .then((results) => {
+                                request.execute("DL_Upsert_Fact_Inventory_Summary").then((execResult) => {
+                                    request.execute("DL_INSERT_DIMTIME").then((execResult) => {
+                                        transaction.commit((err) => {
+                                            if (err)
+                                                reject(err);
+                                            else
+                                                resolve(results);
+                                        });
+                                    }).catch((error) => {
+                                        transaction.rollback((err) => {
+                                            console.log("rollback")
+                                            if (err)
+                                                reject(err)
+                                            else
+                                                reject(error);
+                                        });
+                                    })
+                                }).catch((error) => {
+                                    transaction.rollback((err) => {
+                                        console.log("rollback")
+                                        if (err)
+                                            reject(err)
+                                        else
+                                            reject(error);
+                                    });
+                                })
+                            })
+                            .catch((error) => {
+                                transaction.rollback((err) => {
+                                    console.log("rollback");
+                                    if (err)
+                                        reject(err)
+                                    else
+                                        reject(error);
+                                });
+                            });
+                    })
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        })
+            .catch((err) => {
+                reject(err);
+            })
+    }
+}
