0d1c8acdb7d5b32b73e805d46202056617bcf9d1
jackyrusly <jacky.rusly@CARTIER.moonlay.jakarta>
2017-05-31 14:25:33 +0700

    Revert "Update Fabric Quality Control"
    This reverts commit 125a7b857c44b8db6a9485faa00d879714f245b4.

--- src/managers/production/finishing-printing/fabric-quality-control-manager.js
+++ src/managers/production/finishing-printing/fabric-quality-control-manager.js
@@ -288,7 +288,8 @@ module.exports = class FabricQualityControlManager extends BaseManager {
         var _defaultFilter = {
             _deleted: false
         };
-        var kanbanCodeFilter = {};
+        var productionOrderNoFilter = {};
+        var cartNoFilter = {};
         var productionOrderTypeFilter = {};
         var shiftImFilter = {};
         var dateFromFilter = {};
@@ -296,11 +297,15 @@ module.exports = class FabricQualityControlManager extends BaseManager {
         var query = {};
 
         var dateFrom = info.dateFrom ? (new Date(info.dateFrom)) : (new Date(1900, 1, 1));
-        var dateTo = info.dateTo ? (new Date(info.dateTo)) : (new Date());
+        var dateTo = info.dateTo ? (new Date(info.dateTo + "T23:59")) : (new Date());
         var now = new Date();
 
-        if (info.kanbanCode && info.kanbanCode != '') {
-            kanbanCodeFilter = { 'kanbanCode': info.kanbanCode };
+        if (info.productionOrderNo && info.productionOrderNo != '') {
+            productionOrderNoFilter = { 'productionOrderNo': info.productionOrderNo };
+        }
+
+        if (info.cartNo && info.cartNo != '') {
+            cartNoFilter = { 'cartNo': info.cartNo };
         }
 
         if (info.productionOrderType && info.productionOrderType != '') {
@@ -313,12 +318,12 @@ module.exports = class FabricQualityControlManager extends BaseManager {
 
         var filterDate = {
             "dateIm": {
-                $gte: dateFrom,
-                $lte: dateTo
+                $gte: new Date(dateFrom),
+                $lte: new Date(dateTo)
             }
         };
 
-        query = { '$and': [_defaultFilter, kanbanCodeFilter, productionOrderTypeFilter, shiftImFilter, filterDate] };
+        query = { '$and': [_defaultFilter, productionOrderNoFilter, cartNoFilter, productionOrderTypeFilter, shiftImFilter, filterDate] };
 
         return this._createIndexes()
             .then((createIndexResults) => {
