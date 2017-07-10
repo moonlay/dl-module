7075c9a70a442591de9964c364f5df941a1356e7
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-10 17:11:39 +0700

    add garment product

---
+++ test/master/garment-product/index.js
@@ -0,0 +1,12 @@
+describe("BASIC CRUD SCENARIOS", function() {
+    require("./basic");
+});
+
+describe("GET SCENARIOS", function() {
+    require("./get-scenario");
+});
+
+describe("READ SCENARIOS", function() {
+    require("./read-by-id");
+});
+
