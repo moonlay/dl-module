fea978c4b73529c4c2c7d33013f0c8f3c47ca4c4
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-27 17:25:56 +0700

    add unit test generate pdf

--- test/garment-purchasing/purchase-order-external/create.js
+++ test/garment-purchasing/purchase-order-external/create.js
@@ -107,3 +107,24 @@ it('#04. purchase-orders supplier & currency should be the same with one in purc
             done(e);
         });
 });
+
+it('#05. should success when generate pdf purchase-order-external', function (done) {
+    purchaseOrderExternalManager.dpf(purchaseOrderExternal, 7)
+        .then(results => {
+            done();
+        })
+        .catch(e => {
+            done(e);
+        });
+});
+
+it('#06. should success when generate pdf  purchase-order-external', function (done) {
+    purchaseOrderExternal.category.code = "FAB";
+    purchaseOrderExternalManager.pdf(purchaseOrderExternal, 7)
+        .then(results => {
+            done();
+        })
+        .catch(e => {
+            done(e);
+        });
+});
