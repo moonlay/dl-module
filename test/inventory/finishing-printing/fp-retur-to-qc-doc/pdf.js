604372330b492cbc6fb8dddd345c6a1f818a43f7
achensetyawati <achenzetya@gmail.com>
2017-08-01 08:30:36 +0700

    fix unit test

--- test/inventory/finishing-printing/fp-retur-to-qc-doc/pdf.js
+++ test/inventory/finishing-printing/fp-retur-to-qc-doc/pdf.js
@@ -55,7 +55,7 @@ it(`#02. should success when get created data with id`, function (done) {
 it('#04. should success when create pdf', function (done) {
     var query = {};
 
-    instanceManager.pdf(createdData,7)
+    instanceManager.pdf(createdData._id,7)
         .then((pdfData) => {
             done();
         }).catch((e) => {
