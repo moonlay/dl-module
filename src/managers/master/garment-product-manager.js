e8d7f18d5021a5241d92db48b25734fcb5172b31
indriHutabalian <indri.hutabalian@moonlay.com>
2017-08-08 10:40:35 +0700

    fix upload

--- src/managers/master/garment-product-manager.js
+++ src/managers/master/garment-product-manager.js
@@ -216,7 +216,7 @@ module.exports = class GarmentProductManager extends BaseManager {
                                             errorMessage = errorMessage + "Satuan tidak terdaftar di Master Satuan, ";
                                         }
 
-                                        // var _currency = currencies.find(currency => currency.code === data[i]["currency"])
+                                        var _currency = currencies.find(currency => currency.code === data[i]["currency"])
                                         // if (!_currency) {
                                         //     errorMessage = errorMessage + "Mata Uang tidak terdaftar di Master Mata Uang";
                                         // }
@@ -224,7 +224,7 @@ module.exports = class GarmentProductManager extends BaseManager {
                                         if (errorMessage !== "") {
                                             dataError.push({ "code": data[i]["code"], "name": data[i]["name"], "uom": data[i]["uom"], "currency": data[i]["currency"], "price": data[i]["price"], "tags": data[i]["tags"], "description": data[i]["description"], "properties": data[i]["properties"], "Error": errorMessage });
                                         } else {
-                                            // data[i]["currency"] = _currency;
+                                            data[i]["currency"] = _currency;
                                             data[i]["uom"] = _uom;
                                         }
                                     }
