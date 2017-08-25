d25f49123a1d6b22b5d06d041af2aea18faa5690
Leslie Aula <cristiano.ulo@gmail.com>
2017-08-25 15:12:30 +0700

    update kanban

--- src/etl/production/fact-kanban-etl-manager.js
+++ src/etl/production/fact-kanban-etl-manager.js
@@ -19,8 +19,9 @@ const SELECTED_FIELDS = {
     "instruction.code": 1,
     "instruction.name": 1,
     "instruction.steps._id": 1,
+    "instruction.steps.code": 1,
     "instruction.steps.process": 1,
-    "instruction.steps.proccessArea": 1,
+    "instruction.steps.processArea": 1,
     "instruction.steps.machine.code": 1,
     "instruction.steps.machine.name": 1,
     "instruction.steps.machine.monthlyCapacity": 1,
@@ -32,14 +33,12 @@ const SELECTED_FIELDS = {
 require("mongodb-toolkit");
 
 var KanbanManager = require("../../managers/production/finishing-printing/kanban-manager");
-var DailyOperationManager = require("../../managers/production/finishing-printing/daily-operation-manager");
 
 module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
     constructor(db, user, sql) {
         super(db, user);
         this.sql = sql;
         this.kanbanManager = new KanbanManager(db, user);
-        this.dailyOperationManager = new DailyOperationManager(db, user);
         this.migrationLog = this.db.collection("migration-log");
     }
 
@@ -58,7 +57,7 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                 var finishedDate = new Date();
                 var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
                 var updateLog = {
-                    description: "Fact Kanban Operation from MongoDB to Azure DWH",
+                    description: "Fact Kanban from MongoDB to Azure DWH",
                     start: startedDate,
                     finish: finishedDate,
                     executionTime: spentTime + " minutes",
@@ -70,7 +69,7 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                 var finishedDate = new Date();
                 var spentTime = moment(finishedDate).diff(moment(startedDate), "minutes");
                 var updateLog = {
-                    description: "Fact Kanban Operation from MongoDB to Azure DWH",
+                    description: "Fact Kanban from MongoDB to Azure DWH",
                     start: startedDate,
                     finish: finishedDate,
                     executionTime: spentTime + " minutes",
@@ -82,7 +81,7 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
 
     timestamp() {
         return this.migrationLog.find({
-            description: "Fact Kanban Operation from MongoDB to Azure DWH",
+            description: "Fact Kanban from MongoDB to Azure DWH",
             status: "Successful"
         }).sort({ finish: -1 }).limit(1).toArray()
     }
@@ -94,10 +93,10 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
             "_updatedDate": {
                 $gte: timestamp
             }
-        }, SELECTED_FIELDS).toArray()
-            .then((kanbans) => this.joinDailyOperation(kanbans))
+        }, SELECTED_FIELDS).toArray();
     }
 
+    /*
     joinDailyOperation(kanbans) {
         var joinDailyOperations = kanbans.map((kanban) => {
             var kanbanCurrentStepId = kanban.instruction && kanban.instruction.steps.length > 0 && kanban.instruction.steps[Math.abs(kanban.currentStepIndex === kanban.instruction.steps.length ? kanban.currentStepIndex - 1 : kanban.currentStepIndex)]._id ? kanban.instruction.steps[Math.abs(kanban.currentStepIndex === kanban.instruction.steps.length ? kanban.currentStepIndex - 1 : kanban.currentStepIndex)]._id : null;
@@ -114,7 +113,6 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                     "input": 1,
                     "step.proccessArea": 1
                 }).limit(1).toArray();
-
             return getDailyOperations.then((dailyOperations) => {
                 var arr = dailyOperations.map((dailyOperation) => {
                     kanban.dailyOperationMachine = dailyOperation.machine && dailyOperation.machine.name ? dailyOperation.machine.name : null;
@@ -124,7 +122,6 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                     kanban.dailyOperationProcessArea = dailyOperation.step && dailyOperation.step.proccessArea ? dailyOperation.step.proccessArea : null;
                     return kanban;
                 });
-
                 if (arr.length === 0) {
                     kanban.dailyOperationMachine = null;
                     kanban.dateInput = null;
@@ -136,51 +133,52 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                 return Promise.resolve(arr);
             });
         });
-
         return Promise.all(joinDailyOperations)
             .then(((joinDailyOperation) => {
                 return Promise.resolve([].concat.apply([], joinDailyOperation));
             }));
     }
+    */
 
     transform(data) {
         var result = data.map((item) => {
             var kanban = item.instruction && item.instruction.steps && item.instruction.steps.length > 0 ? item : null;
 
             if (kanban) {
+                /*
                 var stepIndex = kanban.currentStepIndex === kanban.instruction.steps.length ? Math.abs(kanban.currentStepIndex - 1) : Math.abs(kanban.currentStepIndex);
                 var kanbanSteps = kanban.instruction.steps[stepIndex];
-                return {
-                    deleted: `'${kanban._deleted}'`,
-                    kanbanCode: kanban.code ? `'${kanban.code}'` : null,
-                    kanbanDate: kanban._createdDate ? `'${moment(kanban._createdDate).subtract(7, "hours").format("L")}'` : null,
-                    productionOrderNo: kanban.productionOrder && kanban.productionOrder.orderNo ? `'${kanban.productionOrder.orderNo}'` : null,
-                    kanbanGrade: kanban.grade ? `'${kanban.grade}'` : null,
-                    kanbanCartNumber: kanban.cart && kanban.cart.cartNumber ? `'${kanban.cart.cartNumber}'` : null,
-                    kanbanCartQuantity: kanban.cart && kanban.cart.qty ? `${kanban.cart.qty}` : null,
-                    kanbanInstructionId: kanban.instruction && kanban.instruction._id ? `'${kanban.instruction._id}'` : null,
-                    kanbanInstructionCode: kanban.instruction && kanban.instruction.code ? `'${kanban.instruction.code}'` : null,
-                    kanbanInstructionName: kanban.instruction && kanban.instruction.name ? `'${kanban.instruction.name}'` : null,
-                    kanbanStepsId: kanbanSteps._id ? `'${kanbanSteps._id}'` : null,
-                    kanbanStepsCode: kanbanSteps.code ? `'${kanbanSteps.code}'` : null,
-                    kanbanStepsName: kanbanSteps.process ? `'${kanbanSteps.process}'` : null,
-                    machineCode: kanbanSteps.machine && kanbanSteps.machine.code ? `'${kanbanSteps.machine.code}'` : null,
-                    machineName: kanbanSteps.machine && kanbanSteps.machine.name ? `'${kanbanSteps.machine.name}'` : null,
-                    machineMonthlyCapacity: kanbanSteps.machine && kanbanSteps.machine.monthlyCapacity ? `${kanbanSteps.machine.monthlyCapacity}` : null,
-                    deadline: kanbanSteps.deadline ? `'${moment(kanbanSteps.deadline).subtract(7, "hours").format("L")}'` : null,
-                    inputQuantity: `${kanban.inputQuantity}`,
-                    currentStepIndex: `${kanban.currentStepIndex}`,
-                    processArea: kanban.dailyOperationProcessArea ? `'${kanban.dailyOperationProcessArea}'` : null,
-                    isComplete: `'${kanban.isComplete}'`,
-                    stepsLength: `${kanban.instruction.steps.length}`,
-                    dailyOperationMachine: kanban.dailyOperationMachine ? `'${kanban.dailyOperationMachine}'` : null,
-                    dateInput: kanban.dateInput ? `'${moment(kanban.dateInput).subtract(7, "hours").format("L")}'` : null,
-                    timeInput: kanban.timeInput ? `'${moment(kanban.timeInput).format("HH:mm:ss")}'` : null,
-                    dateTimeInput: kanban.timeInput && kanban.dateInput ? `'${moment(kanban.dateInput).subtract(7, "hours").format("L")} ${moment(kanban.timeInput).format("HH:mm:ss")}'` : null,
-                    salesContractNo: kanban.productionOrder && kanban.productionOrder.salesContractNo ? `'${kanban.productionOrder.salesContractNo}'` : null
-                }
+                */
+                var index = 0;
+                var kanbanData = kanban.instruction.steps.map((step) => {
+                    return {
+                        deleted: `'${kanban._deleted}'`,
+                        kanbanCode: kanban.code ? `'${kanban.code}'` : null,
+                        kanbanDate: kanban._createdDate ? `'${moment(kanban._createdDate).add(7, "hours").format("L")}'` : null,
+                        productionOrderNo: kanban.productionOrder && kanban.productionOrder.orderNo ? `'${kanban.productionOrder.orderNo}'` : null,
+                        kanbanGrade: kanban.grade ? `'${kanban.grade}'` : null,
+                        kanbanCartNumber: kanban.cart && kanban.cart.cartNumber ? `'${kanban.cart.cartNumber}'` : null,
+                        kanbanCartQuantity: kanban.cart && kanban.cart.qty ? `${kanban.cart.qty}` : null,
+                        kanbanInstructionId: kanban.instruction && kanban.instruction._id ? `'${kanban.instruction._id}'` : null,
+                        kanbanInstructionCode: kanban.instruction && kanban.instruction.code ? `'${kanban.instruction.code}'` : null,
+                        kanbanInstructionName: kanban.instruction && kanban.instruction.name ? `'${kanban.instruction.name}'` : null,
+                        kanbanStepsId: step._id ? `'${step._id}'` : null,
+                        kanbanStepsCode: step.code ? `'${step.code}'` : null,
+                        kanbanStepsName: step.process ? `'${step.process}'` : null,
+                        machineCode: step.machine && step.machine.code ? `'${step.machine.code}'` : null,
+                        machineName: step.machine && step.machine.name ? `'${step.machine.name}'` : null,
+                        machineMonthlyCapacity: step.machine && step.machine.monthlyCapacity ? `${step.machine.monthlyCapacity}` : null,
+                        deadline: step.deadline ? `'${moment(step.deadline).add(7, "hours").format("L")}'` : null,
+                        currentStepIndex: `${kanban.currentStepIndex}`,
+                        processArea: step.processArea ? `'${step.processArea}'` : null,
+                        isComplete: `'${kanban.isComplete}'`,
+                        stepsLength: `${kanban.instruction.steps.length}`,
+                        stepIndex: index++,
+                        salesContractNo: kanban.productionOrder && kanban.productionOrder.salesContractNo ? `'${kanban.productionOrder.salesContractNo}'` : null
+                    }
+                });
+                return [].concat.apply([], kanbanData);
             }
-
         });
         return Promise.resolve([].concat.apply([], result));
     };
@@ -216,12 +214,12 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
 
                         for (var item of data) {
                             if (item) {
-                                var queryString = `\nSELECT ${item.deleted}, ${item.kanbanCode}, ${item.kanbanDate}, ${item.productionOrderNo}, ${item.kanbanGrade}, ${item.kanbanCartNumber}, ${item.kanbanCartQuantity}, ${item.kanbanInstructionId}, ${item.kanbanInstructionCode}, ${item.kanbanInstructionName}, ${item.kanbanStepsId}, ${item.kanbanStepsCode}, ${item.kanbanStepsName}, ${item.machineCode}, ${item.machineName}, ${item.machineMonthlyCapacity}, ${item.deadline}, ${item.currentStepIndex}, ${item.processArea}, ${item.isComplete}, ${item.stepsLength}, ${item.dailyOperationMachine}, ${item.dateInput},  ${item.inputQuantity}, ${item.timeInput}, ${item.dateTimeInput}, ${item.salesContractNo} UNION ALL `;
+                                var queryString = `\nSELECT ${item.deleted}, ${item.kanbanCode}, ${item.kanbanDate}, ${item.productionOrderNo}, ${item.kanbanGrade}, ${item.kanbanCartNumber}, ${item.kanbanCartQuantity}, ${item.kanbanInstructionId}, ${item.kanbanInstructionCode}, ${item.kanbanInstructionName}, ${item.kanbanStepsId}, ${item.kanbanStepsCode}, ${item.kanbanStepsName}, ${item.machineCode}, ${item.machineName}, ${item.machineMonthlyCapacity}, ${item.deadline}, ${item.currentStepIndex}, ${item.processArea}, ${item.isComplete}, ${item.stepsLength}, ${item.stepIndex}, ${item.salesContractNo} UNION ALL `;
                                 sqlQuery = sqlQuery.concat(queryString);
-                                if (count % 500 === 0) {
+                                if (count % 1000 === 0) {
                                     sqlQuery = sqlQuery.substring(0, sqlQuery.length - 10);
                                     command.push(this.insertQuery(request, sqlQuery));
-                                    sqlQuery = "";
+                                    sqlQuery = "INSERT INTO [DL_Fact_Kanban_Temp] ";
                                 }
                                 console.log(`add data to query  : ${count}`);
                                 count++;
@@ -236,7 +234,7 @@ module.exports = class FactMonitoringKanbanEtlManager extends BaseManager {
                         this.sql.multiple = true;
 
                         // var fs = require("fs");
-                        // var path = "C:\\Users\\leslie.aula\\Desktop\\kanban.txt";
+                        // var path = "C:\\Users\\jacky.rusly\\Desktop\\kanban.txt";
 
                         // fs.writeFile(path, sqlQuery, function (error) {
                         //     if (error) {
