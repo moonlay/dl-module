5da2216f2e9d5fc429b086ade5e511effa3c5613
indriHutabalian <indri.hutabalian@moonlay.com>
2017-09-05 16:32:29 +0700

    add validation delivery order useCustoms==true

--- test/garment-purchasing/customs/validate.js
+++ test/garment-purchasing/customs/validate.js
@@ -42,181 +42,204 @@ it("#01. should error when create new data with empty data", function (done) {
 it("#02. should error when create new data with no exist data supplier", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                data.supplierId = "supplierId";
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with no exist data supplier");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('supplier');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            data.supplierId = "supplierId";
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with no exist data supplier");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('supplier');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#03. should error when create new data with no exist data currency", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                data.currencyId = "currencyId";
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with no exist data currency");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('currency');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            data.currencyId = "currencyId";
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with no exist data currency");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('currency');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#04. should error when create new data with customs date more than this day", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                data.customsDate = moment(dateAfter).format('YYYY-MM-DD');
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with customs date more than this day");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('customsDate');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            data.customsDate = moment(dateAfter).format('YYYY-MM-DD');
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with customs date more than this day");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('customsDate');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#05. should error when create new data with validation date more than this day", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                data.validateDate = moment(dateAfter).format('YYYY-MM-DD');
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with validation date more than this day");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('validateDate');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            data.validateDate = moment(dateAfter).format('YYYY-MM-DD');
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with validation date more than this day");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('validateDate');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#06. should error when create new data with validation date less than customs date", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                var date = new Date();
-                date = date.setDate(date.getDate() - 2);
-                data.validateDate = moment(date).format('YYYY-MM-DD');
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with validation date less than customs date");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('validateDate');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            var date = new Date();
+            date = date.setDate(date.getDate() - 2);
+            data.validateDate = moment(date).format('YYYY-MM-DD');
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with validation date less than customs date");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('validateDate');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#07. should error when create new data with bruto less than netto", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                data.bruto = 15;
-                data.netto = 20;
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with bruto less than netto");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('netto');
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            data.bruto = 15;
+            data.netto = 20;
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with bruto less than netto");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('netto');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#08. should error when create new data with no exist data delivery order", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                var tamp = [];
-                for(var a of data.deliveryOrders){
-                    a._id = "id";
-                    tamp.push(a);
-                }
-                data.deliveryOrders = tamp;
-                customsManager.create(data)
-                    .then((id) => {
-                        done("should error when create new data with bruto less than netto");
-                    })
-                    .catch((e) => {
-                        e.name.should.equal("ValidationError");
-                        e.should.have.property("errors");
-                        e.errors.should.instanceof(Object);
-                        e.errors.should.have.property('deliveryOrders');
-                        e.errors.deliveryOrders.should.instanceof(Array);
-                        done();
-                    });
-            })
-            .catch((e) => {
-                done(e);
-            });
+            var tamp = [];
+            for (var a of data.deliveryOrders) {
+                a._id = "id";
+                tamp.push(a);
+            }
+            data.deliveryOrders = tamp;
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with bruto less than netto");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('deliveryOrders');
+                    e.errors.deliveryOrders.should.instanceof(Array);
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
 
 it("#09. should error when create new data with same no, supplier, customs date, validation date", function (done) {
     customsDataUtil.getNewData()
         .then((data) => {
-                customsManager.create(data)
-                    .then((id1) => {
-                        customsManager.create(data)
-                            .then((id2) => {
-                                done("should error when create new data with same no, supplier, customs date, validation date");
-                            })
-                            .catch((e) => {
-                                e.name.should.equal("ValidationError");
-                                e.should.have.property("errors");
-                                e.errors.should.instanceof(Object);
-                                e.errors.should.have.property('no');
-                                done();
-                            });
-                    })
-                    .catch((e) => {
-                        done(e);
-                    });
+            customsManager.create(data)
+                .then((id1) => {
+                    customsManager.create(data)
+                        .then((id2) => {
+                            done("should error when create new data with same no, supplier, customs date, validation date");
+                        })
+                        .catch((e) => {
+                            e.name.should.equal("ValidationError");
+                            e.should.have.property("errors");
+                            e.errors.should.instanceof(Object);
+                            e.errors.should.have.property('no');
+                            done();
+                        });
+                })
+                .catch((e) => {
+                    done(e);
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
+});
+
+it("#10. should error when create new data with data delivery order useCostumes = false", function (done) {
+    customsDataUtil.getNewData()
+        .then((data) => {
+            data.deliveryOrders.map((deliveryOrder) => {
+                deliveryOrder.useCustoms = false;
             })
-            .catch((e) => {
-                done(e);
-            });
+            customsManager.create(data)
+                .then((id) => {
+                    done("should error when create new data with data delivery order useCostumes = false");
+                })
+                .catch((e) => {
+                    e.name.should.equal("ValidationError");
+                    e.should.have.property("errors");
+                    e.errors.should.instanceof(Object);
+                    e.errors.should.have.property('deliveryOrders');
+                    done();
+                });
+        })
+        .catch((e) => {
+            done(e);
+        });
 });
