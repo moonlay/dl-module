29a86896aef8dc204208d7744f1319f7e0347467
achensetyawati <achenzetya@gmail.com>
2017-09-08 08:13:22 +0700

    unit test

--- test/garment-purchasing/delivery-order/index.js
+++ test/garment-purchasing/delivery-order/index.js
@@ -1,10 +1,10 @@
-// describe("BASIC CRUD SCENARIOS", function() {
-//     require("./basic");
-// });
+describe("BASIC CRUD SCENARIOS", function() {
+    require("./basic");
+});
 
-// describe("CREATE SCENARIOS", function() {
-//     require("./create");
-// });
+describe("CREATE SCENARIOS", function() {
+    require("./create");
+});
 
 describe("EXCEL SCENARIOS", function() {
     require("./xls");
