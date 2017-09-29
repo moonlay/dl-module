768a8aeb82f8c0e93c993b178cf7533fff23ac37
AndreaZain <andrea.kinan@gmail.com>
2017-09-29 13:31:22 +0700

    update unit test hliday
    update unit test hliday

--- test/master/holiday/index.js
+++ test/master/holiday/index.js
@@ -1,12 +1,3 @@
 describe("BASIC CRUD SCENARIOS", function() {
     require("./basic");
 });
-
-describe("GET SCENARIOS", function() {
-    require("./get-scenario");
-});
-
-describe("READ SCENARIOS", function() {
-    require("./read-by-id");
-});
-
