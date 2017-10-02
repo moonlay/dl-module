61d1b321562633500a53adfc6c10a3cb5915a69d
achensetyawati <achenzetya@gmail.com>
2017-10-02 10:24:04 +0700

    unit test

--- test/data-util/purchasing/unit-receipt-note-data-util.js
+++ test/data-util/purchasing/unit-receipt-note-data-util.js
@@ -5,17 +5,19 @@ var codeGenerator = require('../../../src/utils/code-generator');
 var unit = require("../master/unit-data-util");
 var supplier = require('../master/supplier-data-util');
 var deliveryOrder = require('./delivery-order-data-util');
+var storageDataUtil = require('../master/storage-data-util');
 
 class UnitReceiptNoteDataUtil {
     getNewData() {
         return helper
             .getManager(UnitReceiptNoteManager)
             .then(manager => {
-                return Promise.all([unit.getTestData(), supplier.getTestData(), deliveryOrder.getNewTestData()])
+                return Promise.all([unit.getTestData(), supplier.getTestData(), deliveryOrder.getNewTestData(),storageDataUtil.getTextileInventoryTestData()])
                     .then(results => {
                         var dataUnit = results[0];
                         var dataSupplier = results[1];
                         var dataDeliveryOrder = results[2];
+                        var storage = results[3];
 
                         var doItems = dataDeliveryOrder.items.map(doItem => {
                             var item = doItem.fulfillments.map(fulfillment => {
