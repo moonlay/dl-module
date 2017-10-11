661cd05fe040422f0520a57a9902cd4f13545cda
achensetyawati <achenzetya@gmail.com>
2017-10-11 10:09:20 +0700

    validation unit test

--- test/purchasing/unit-receipt-note/validator.js
+++ test/purchasing/unit-receipt-note/validator.js
@@ -83,3 +83,111 @@ it('#03. should error when create new data isInventory=true without storage', fu
             done(e);
         });
 });
+
+it('#04. should error when create new data without deliveryOrderId, unitId, supplierId', function (done) {
+    unitReceiptNote.getNewData()
+        .then(data => {
+
+            data.deliveryOrder._id=null;
+            data.unit._id=null;
+            data.supplier._id=null;
+
+            unitReceiptNoteManager.create(data)
+                .then(id => {
+                    done("should error when create new data without deliveryOrderId, unitId, supplierId");
+                })
+                .catch(e => {
+                    try {
+                        e.errors.should.have.property('deliveryOrder');
+                        e.errors.should.have.property('unit');
+                        e.errors.should.have.property('supplier');
+                        done();
+                    }
+                    catch (ex) {
+                        done(ex);
+                    }
+                });
+        })
+        .catch(e => {
+            done(e);
+        });
+});
+
+it('#05. should error when create new data with date < deliveryOrderDate', function (done) {
+    unitReceiptNote.getNewData()
+        .then(data => {
+            var today = new Date();
+            var yesterday = new Date(today);
+            yesterday.setDate(today.getDate() - 1);
+
+            data.deliveryOrder.date=today;
+            data.date=yesterday;
+
+            unitReceiptNoteManager.create(data)
+                .then(id => {
+                    done("should error when create new data with date < deliveryOrderDate");
+                })
+                .catch(e => {
+                    try {
+                        e.errors.should.have.property('date');
+                        done();
+                    }
+                    catch (ex) {
+                        done(ex);
+                    }
+                });
+        })
+        .catch(e => {
+            done(e);
+        });
+});
+
+it('#06. should error when create new data without items', function (done) {
+    unitReceiptNote.getNewData()
+        .then(data => {
+            
+            data.items=[];
+
+            unitReceiptNoteManager.create(data)
+                .then(id => {
+                    done("should error when create new data without items");
+                })
+                .catch(e => {
+                    try {
+                        e.errors.should.have.property('items');
+                        done();
+                    }
+                    catch (ex) {
+                        done(ex);
+                    }
+                });
+        })
+        .catch(e => {
+            done(e);
+        });
+});
+
+it('#07. should error when create new data with deliveredQuantity=0', function (done) {
+    unitReceiptNote.getNewData()
+        .then(data => {
+            
+            data.items[0].deliveredQuantity=0;
+
+            unitReceiptNoteManager.create(data)
+                .then(id => {
+                    done("should error when create new data with deliveredQuantity=0");
+                })
+                .catch(e => {
+                    try {
+                        e.errors.should.have.property('items');
+                        done();
+                    }
+                    catch (ex) {
+                        done(ex);
+                    }
+                });
+        })
+        .catch(e => {
+            done(e);
+        });
+});
