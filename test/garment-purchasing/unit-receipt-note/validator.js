d4beb78c522172e573a6dff636eeacf7e091358b
LuciaWidyana <mo_cia@yahoo.com>
2017-10-23 16:27:39 +0700

    fixGarmentunitreceipt

---
+++ test/garment-purchasing/unit-receipt-note/validator.js
@@ -0,0 +1,89 @@
+var helper = require("../../helper");
+var validator = require('dl-models').validator.master;
+var validatorPurchasing = require('dl-models').validator.purchasing;
+var UnitReceiptNoteManager = require("../../../src/managers/purchasing/unit-receipt-note-manager");
+var unitReceiptNoteManager = null;
+var unitReceiptNote = require("../../data-util/purchasing/unit-receipt-note-data-util");
+var StorageManager = require("../../../src/managers/master/storage-manager");
+var storageManager = null;
+var storage = require("../../data-util/master/storage-data-util");
+
+require("should");
+
+before('#00. connect db', function (done) {
+    helper.getDb()
+        .then(db => {
+            unitReceiptNoteManager = new UnitReceiptNoteManager(db, {
+                username: 'unit-test'
+            });
+            storageManager = new StorageManager(db, {
+                username: 'unit-test'
+            });
+            done();
+        })
+        .catch(e => {
+            done(e);
+        })
+});
+
+var storageData;
+var storageDataId;
+it('#01. should success when create new data storage', function (done) {
+    storage.getNewData()
+        .then((data) => storageManager.create(data))
+        .then((id) => {
+            storageManager.getSingleById(id)
+            .then((data) => {
+                storageData=data;
+                
+            });
+            
+                id.should.be.Object();
+                storageDataId = id;
+                done();
+        })
+        .catch((e) => {
+            done(e);
+        });
+});
+
+it("#02. should success when create new data when useStorage is true", function (done) {
+     unitReceiptNote.getNewData()
+        .then((data) => {
+            data.storageId=storageDataId;
+            data.unit=storageData.unit;
+            data.useStorage=true;
+            unitReceiptNoteManager.create(data)
+            .then((id) => {
+                id.should.be.Object();
+                var createdId = id;
+                done();
+            })
+        })
+        .catch((e) => {
+            done(e);
+        });
+});
+
+it('#03. should error when create new data useStorage=true without storage', function (done) {
+    unitReceiptNote.getNewData()
+        .then(data => {
+            data.useStorage=true;
+            unitReceiptNoteManager.create(data)
+                .then(id => {
+                    done("should error when create new data useStorage=true without storage");
+                })
+                .catch(e => {
+                    try {
+                        e.errors.should.have.property('storage');
+                        done();
+                    }
+                    catch (ex) {
+                        done(ex);
+                    }
+                });
+        })
+        .catch(e => {
+            done(e);
+        });
+});
