9e6b543f510596efecee66fa2c5fb0b51dede7b6
indriHutabalian <indri.hutabalian@moonlay.com>
2017-10-26 16:47:21 +0700

    add method cleanUp

--- src/managers/garment-purchasing/unit-receipt-note-manager.js
+++ src/managers/garment-purchasing/unit-receipt-note-manager.js
@@ -365,4 +365,14 @@ module.exports = class UnitReceiptNoteManager extends BaseManager {
                 }
             });
     }
+
+    cleanUp(input) {
+        var newArr = [];
+        for (var i = 0; i < input.length; i++) {
+            if (input[i]) {
+                newArr.push(input[i]);
+            }
+        }
+        return newArr;
+    }
 };
