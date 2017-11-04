eb3814afe80b8f91bd89c3f8f375141053d6a77a
achensetyawati <achenzetya@gmail.com>
2017-11-04 11:12:32 +0700

    fix unit test

--- test/master/spinning-production-lot/basic.js
+++ test/master/spinning-production-lot/basic.js
@@ -1,7 +1,7 @@
 var options = {
-    manager: require("../../../src/managers/master/spinning-lot-production-manager"),
+    manager: require("../../../src/managers/master/spinning-production-lot-manager"),
     model: require("dl-models").master.SpinningProductionLot,
-    util: require("../../data-util/master/spinning-lot-production-data-util"),
+    util: require("../../data-util/master/spinning-production-lot-data-util"),
     validator: require("dl-models").validator.master.spinningProductionLot,
     createDuplicate: true,
     keys: ["unitId","lot","spinningYarnId","machineId"]
