3b7570e6284f437da1082f57b254a89cbf61907a
indriHutabalian <indri.hutabalian@moonlay.com>
2017-11-06 11:39:37 +0700

    add data util weekly plan

---
+++ test/data-util/garment-master-plan/weekly-plan-data-util.js
@@ -0,0 +1,46 @@
+"use strict";
+var helper = require("../../helper");
+var unit = require("../master/unit-data-util");
+var WeeklyPlanManager = require("../../../src/managers/garment-master-plan/weekly-plan-manager");
+var moment = require("moment");
+
+class WeeklyPlanDataUtil {
+    getNewData() {
+        return Promise.all([unit.getTestData()])
+            .then((results) => {
+                var unit = results[0];
+
+                var items = [];
+                for (var i = 1; i <= 52; i++) {
+                    var startDate =  moment().year(2018).day("Monday").week(i).toDate();
+                    var endDate =  moment().year(2018).day("Friday").week(i).toDate();
+                    items.push({
+                        weekNumber: i,
+                        startDate: startDate,
+                        endDate: endDate,
+                        month: startDate.getMonth()
+                    })
+                }
+                var data = {
+                    unitId: unit._id,
+                    unit: unit,
+                    year: new Date().getFullYear()+1,
+                    items: items
+                };
+                return Promise.resolve(data);
+            });
+    }
+
+    getNewTestData() {
+        return helper
+            .getManager(WeeklyPlanManager)
+            .then((manager) => {
+                return this.getNewData().then((data) => {
+                    return manager.create(data)
+                        .then((id) => manager.getSingleById(id));
+                });
+            });
+    }
+}
+
+module.exports = new WeeklyPlanDataUtil();
