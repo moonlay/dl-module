559e623727572e6a69be48cff6ed8da8309a8949
achensetyawati <achenzetya@gmail.com>
2017-11-29 14:31:05 +0700

    unit test

--- test/garment-master-plan/standard-hour/index.js
+++ test/garment-master-plan/standard-hour/index.js
@@ -5,3 +5,7 @@ describe("BASIC CRUD SCENARIOS", function() {
 describe("VALIDATION SCENARIOS", function() {
     require("./validate");
 });
+
+describe("GET SCENARIOS", function() {
+    require("./get-scenario");
+});
