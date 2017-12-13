a55ed7be7480e1c067029acda3c5e790f67e155c
indriHutabalian <indri.hutabalian@moonlay.com>
2017-12-13 16:19:30 +0700

    get field currency

--- src/managers/garment-purchasing/purchase-order-manager.js
+++ src/managers/garment-purchasing/purchase-order-manager.js
@@ -365,6 +365,7 @@ module.exports = class PurchaseOrderManager extends BaseManager {
                         "items.productId": "$items.productId",
                         "items.defaultQuantity": "$items.defaultQuantity",
                         "items.defaultUom": "$items.defaultUom",
+                        "items.currency": "$items.currency",
                         "items.budgetPrice": "$items.budgetPrice",
                         "items.isPosted": "$items.isPosted",
                         "items.isClosed": "$items.isClosed",
