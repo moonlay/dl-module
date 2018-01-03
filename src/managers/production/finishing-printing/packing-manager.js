d5413b480e196bd51c2f87874f1d2395148d854e
baguswidypriyono <kacagatot89@gmail.com>
2018-01-03 14:16:30 +0700

    ubah offset
    ubah offset

--- src/managers/production/finishing-printing/packing-manager.js
+++ src/managers/production/finishing-printing/packing-manager.js
@@ -409,8 +409,10 @@ getQcgudangReport(query ){
 
 
         return new Promise((resolve, reject) => {
-               var date = {
+              var date = {
                 "date" : {
+                    // "$gte" : (!query || !query.dateFrom ? (new Date("1900-01-01")) : (new Date(`${query.dateFrom} 00:00:00`))),
+                    // "$lte" : (!query || !query.dateTo ? (new Date()) : (new Date(`${query.dateTo} 23:59:59`)))
                $gte: new Date(dateFrom),
                 $lte: new Date(dateTo)
             },
@@ -531,7 +533,6 @@ getQcgudangReport(query ){
             });
         });
     }
-
 
     getXls(result, query) {
         var xls = {};
