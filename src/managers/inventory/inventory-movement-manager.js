a3d706065e6b004847b9093dce7b225608987207
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-03-13 15:08:32 +0700

    [Update] Inventory Movement Get Date From I Document & Fix Offset

--- src/managers/inventory/inventory-movement-manager.js
+++ src/managers/inventory/inventory-movement-manager.js
@@ -210,12 +210,25 @@ module.exports = class InventoryMovementManager extends BaseManager {
         return this.inventoryDocumentCollection.find({
             _deleted: false,
             date: query
-        }, { "referenceNo": 1 }).toArray()
+        }, { "referenceNo": 1, "date": 1 }).toArray()
             .then((data) => {
-                var referenceNumbers = data.map((inventoryDocument) => {
-                    return inventoryDocument.referenceNo;
-                })
-                return Promise.resolve(referenceNumbers)
+                var result = {
+                    referenceNumbers: [],
+                    inventoryDocumentsDate: []
+                };
+
+                for (let d of data) {
+                    result.referenceNumbers.push(d.referenceNo);
+                    result.inventoryDocumentsDate.push({
+                        referenceNo: d.referenceNo,
+                        date: d.date
+                    });
+                }
+
+                // var referenceNumbers = data.map((inventoryDocument) => {
+                //     return { referenceNo: inventoryDocument.referenceNo, date: date };
+                // })
+                return Promise.resolve(result)
             })
     }
 
@@ -228,8 +241,11 @@ module.exports = class InventoryMovementManager extends BaseManager {
 
         var dateFrom = info.dateFrom ? (new Date(info.dateFrom)) : (new Date(1900, 1, 1));
         var dateTo = info.dateTo ? (new Date(info.dateTo + "T23:59")) : (new Date());
-
+        dateFrom.setHours(dateFrom.getHours() - info.offset);
+        dateTo.setHours(dateTo.getHours() - info.offset);
+        
         var filterMovement = {};
+        let inventoryDocumentsDate = [];
 
         if (info.storageId)
             filterMovement.storageId = new ObjectId(info.storageId);
@@ -246,11 +262,14 @@ module.exports = class InventoryMovementManager extends BaseManager {
         }
 
         return this.getReferenceNo(dateQuery)
-            .then((referenceNumbers) => {
+            .then((result) => {
                 filterMovement.referenceNo = {
-                    $in: referenceNumbers
+                    $in: result.referenceNumbers
                 };
-                var data = this._createIndexes()
+
+                inventoryDocumentsDate = result.inventoryDocumentsDate;
+
+                return this._createIndexes()
                     .then((createIndexResults) => {
                         query = { '$and': [_defaultFilter, filterMovement] };
                         return !info.xls ?
@@ -263,9 +282,17 @@ module.exports = class InventoryMovementManager extends BaseManager {
                                 .page(info.page, info.size)
                                 .order(order)
                                 .execute();
+                    })
+                    .then(response => {
+                        for (let d of response.data) {
+                            let inventoryDocument = inventoryDocumentsDate.find(p => p.referenceNo == d.referenceNo);
+
+                            d.date = inventoryDocument.date;
+                        }
+
+                        return Promise.resolve(response);
                     });
-                return Promise.resolve(data);
-            })
+            });
     }
 
     getXls(result, filter) {
