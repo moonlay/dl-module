636b5d3191d352822269389449085c35331e2a1e
adityahenanda <henanda.aditya@gmail.com>
2018-05-02 17:59:51 +0700

    update inventory movement
    update inventory movement

--- test/data-util/inventory/inventory-summary-data-util.js
+++ test/data-util/inventory/inventory-summary-data-util.js
@@ -26,7 +26,7 @@ class InventorySummaryDataUtil {
                     storageId: storage._id,
                     uomId: uom._id,
                     quantity: 1000,
-                    stockPlanning:0,
+                    stockPlanning: 0,
 
                 };
 
@@ -47,12 +47,15 @@ class InventorySummaryDataUtil {
             });
     }
 
-    
+
     getNewTestData2() {
         return helper
             .getManager(InventorySummaryManager)
             .then((manager) => {
                 return this.getNewData().then((data) => {
+                    data.productId = "";
+                    data.storageId = "";
+                    data.uomId = "";
                     data.stockPlanning = 1000;
                     return manager.create(data)
                         .then((id) => {
