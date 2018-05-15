882eba0085feb5e3f0fab9b6a3af8eeee680b4da
kristika <kristika.sachi@gmail.com>
2018-05-15 11:28:53 +0700

    Update module
    Add manager getDurationPoExtDo

--- test/garment-purchasing/purchase-order/report.js
+++ test/garment-purchasing/purchase-order/report.js
@@ -374,3 +374,257 @@ it('#22. should success when get data for Excel Report using both dateFrom, date
             done(e);
         });
 });
+
+var resultForExcelTest = {};
+it('#23. should success when get data with Start Date, End Date and Duration 0-30 days', function (done) {
+    var query = {};
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "0-30 hari";
+
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#24. should success when get data with date, unit and Duration 31-60 days', function (done) {
+    var query = {};
+    query.unitId= createdData.unit._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "31-60 hari";
+
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#25. should success when get data with Start Date, End Date and Duration >60 days', function (done) {
+    var query = {};
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "> 60 hari";
+
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#26. should success when get data for Excel Report', function (done) {
+    var query = {};
+    query.duration = "0-30 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#27. should success when get data for Excel Report using dateFrom only', function (done) {
+    var query = {};
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "31-60 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#28. should success when get data for Excel Report using dateTo only', function (done) {
+    var query = {};
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "> 60 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#29. should success when get data for Excel Report using both dateFrom and dateTo', function (done) {
+    var query = {};
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "0-30 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#30. should success when get data with date and unit', function (done) {
+    var query = {};
+    query.unitId = createdData.unit._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+  
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#31. should success when get data with date and unit', function (done) {
+    var query = {};
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+  
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+
+it('#32. should success when get data with date, user, unit and Duration 0-30 days', function (done) {
+    var query = {};
+    query.unitId= createdData.unit._id;
+    query.user= createdData._createdBy;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "0-30 hari";
+
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#33. should success when get data for Excel Report using both dateFrom, dateTo and unit', function (done) {
+    var query = {};
+    query.unitId = createdData.unit._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "0-30 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+it('#34. should success when get data with date, user, unit and Duration 31-60 days', function (done) {
+    var query = {};
+    query.unitId= createdData.unit._id;
+    query.user= createdData._createdBy;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "31-60 hari";
+
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+
+it('#35. should success when get data for Excel Report using both dateFrom, dateTo and unit', function (done) {
+    var query = {};
+    query.unitId = createdData.unit._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.duration = "31-60 hari";
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+it('#36. should success when get data with date and supplier', function (done) {
+    var query = {};
+    query.supplierId = createdData.items[0].supplier._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+  
+    manager.getDurationPoExtDo(query)
+        .then(result => {
+            var po = result;
+            resultForExcelTest.info = result;
+            po.should.instanceof(Array);
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
+it('#37. should success when get data for Excel Report using both dateFrom, dateTo and supplier', function (done) {
+    var query = {};
+    query.supplierId = createdData.items[0].supplier._id;
+    query.dateFrom = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+    query.dateTo = new Date(createdData.items[0].purchaseOrderExternal._createdDate);
+
+    manager.getXlsDurationPoExtDo(resultForExcelTest, query)
+        .then(xlsData => {             
+            xlsData.should.have.property('data');
+            xlsData.should.have.property('options');
+            xlsData.should.have.property('name');
+            done();
+        }).catch(e => {
+            done(e);
+        });
+});
